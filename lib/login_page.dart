0447b4535da129681b2615542c99e5dccfb72105
Daniel <turutadaniel20@gmail.com>
2023-11-17 20:30:26 +0200

    Login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:unihack/main.dart';
-// Assuming you have a main_page.dart file
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,22 +12,65 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Spacer(),
             const Text('Welcome!',
             style: TextStyle(
-              fontSize: 80,
+              fontSize: 60,
               fontWeight: FontWeight.bold,
               ),
             ),
+            const Padding(padding: EdgeInsets.all(50)),
+            Container(
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.blue),
+              ),
+            child: const TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.black,
+                  ),
+                ),
+                contentPadding: EdgeInsets.all(15.0),
+              ),
+            ),
+            ),
             const SizedBox(height: 20),
-            const Padding(padding: EdgeInsets.all(20)),
+            Container(
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.blue),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue[100],
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onPressed: () {
                 // Navigate to the main page
                 Navigator.pushReplacement(
@@ -36,8 +78,17 @@ class _LoginPageState extends State<LoginPage> {
                   MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Main Page',)),
                 );
               },
-              child: const Text('Go to Main Page'),
+              child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+              ),
             ),
+
+            Spacer(),
           ],
         ),
       ),
